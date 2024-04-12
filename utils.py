99ed7066810fafdf989ca799660c6e5a7179241a
Pablo Rendon <pablorendon@mac.com>
2024-04-11 22:05:32 -1000

    now running get_all_network_clients here to add it to system prompt

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
+from tool_functions import get_all_network_clients
+
+device_map = get_all_network_clients()
+
 def generate_messages(messages: list, query: str) -> list:
     formated_messages = [
         {
             'role': 'system',
-            'content': 'you are a network and home device troubleshooting expert that is able to call the function in your tools library to reference devices on the network, idenify them by thier given properties and able to use the functions to interacte with the devices for control and troubleshooting purposes. See whats on the network by using the "get_all_network_clients" function. Do not assume or make up any information.'
+            'content': 'You are a network and home device troubleshooting expert that is able to use the device map provided here to reference devices on the network, idenify them by thier given properties and able to use the functions in your tools to interacte with the devices for control and troubleshooting purposes. Do not assume or make up any information. Device Map: ' + device_map
         }
     ]
     for m in messages:
@@ -11,7 +15,7 @@ def generate_messages(messages: list, query: str) -> list:
             'content': m['query']
         })
         formated_messages.append({
-            'role': 'system',
+            'role': 'assistant',
             'content': m['response']
         })
     formated_messages.append(
@@ -21,3 +25,7 @@ def generate_messages(messages: list, query: str) -> list:
         }
     )
     return formated_messages
+
+
+if __name__ == '__main__':
+    print(device_map)
